f4b63949947e684adc0c88611390f045c24e2aa5
Alexander <galexv@umich.edu>
2016-06-16 01:31:37 -0400

    Added some exercises and solutions, fixed some slides.
    More to follow.

---
+++ talk/solutions/ex6_5/simulation.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <alps/mc/mcbase.hpp>
+
+class MySimulation : public alps::mcbase {
+  private:
+    int istep_;
+    int maxcount_;
+    bool verbose_;
+
+  public:
+    MySimulation(const parameters_type& params, std::size_t seed_offset=0);
+
+    void renamed_update();
+    void measure();
+    double fraction_completed() const;
+
+    static parameters_type& define_parameters(parameters_type&);        
+
+    int count() { return istep_; }
+};
